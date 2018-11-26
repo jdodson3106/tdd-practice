012674f35412d4d09dfe6db778d0738451da2dee
Justin Dodson <justin@89websolutions.com>
2018-11-26 11:07:14 -0600

    Updated functional_test file

--- tdd-book/functional_tests.py
+++ tdd-book/functional_tests.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
 
+# First functional test
+
+"""
+	1. set the browser to open the chrome driver given the executable's PATH
+	2. open the browser to the local server url
+	3. check if 'Django' is in the browser title
+"""
 browser = webdriver.Chrome('/Users/justindodson/Desktop/WebDevelopment/obeyTheTestingGoat/tdd-book/chromedriver')
 browser.get('http://localhost:8000')
 

6066c134d82862473be1ac5ed1b172907372f3ea
Justin Dodson <justin@89websolutions.com>
2018-11-27 20:20:50 -0600

    test case for post request through the view and showing in template

--- tdd-book/functional_tests.py
+++ tdd-book/functional_tests.py
@@ -37,22 +37,24 @@ class NewVisitorTest(unittest.TestCase):
 		input_box.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == 'Buy peackock feathers' for row in rows),
-			'New TO-DO did NOT appear in table'
-			)
-
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish Tests HERE!!!')
+		input_box = self.browser.find_element_by_id('id_new_item')
+		input_box.send_keys('Use peacock feathers to make a fly')
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peackock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 		# The page updates again, and now shows both items on her list
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
-
+		self.fail('Finish Tests HERE!!!')
 		# She visits that URL - her to-do list is still there.
 
 		# Satisfied, she goes back to sleep

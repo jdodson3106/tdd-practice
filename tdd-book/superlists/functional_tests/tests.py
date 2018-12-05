7bbbd80bd61b14d89e208bd5f2817d3b851eb20e
Justin Dodson <justin@89websolutions.com>
2018-12-05 06:34:52 -0600

    implement Bootstrap for styling

--- tdd-book/superlists/functional_tests/tests.py
+++ tdd-book/superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +10,7 @@ import unittest
 
 # First functional test
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	MAX_WAIT = 10
 

941ef7a2ffbc58b04958d1d70057746b972584e0
Justin Dodson <justin@89websolutions.com>
2018-12-06 06:38:36 -0600

    update chromedriver call for staging server

--- tdd-book/superlists/functional_tests/tests.py
+++ tdd-book/superlists/functional_tests/tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		staging_server = os.environ.get('STAGING_SERVER')
 		if staging_server:
-			self.browser = webdriver.Chrome()
+			self.browser = webdriver.Chrome('/usr/local/bin/chromedriver')
 			self.live_server_url = 'http://{}'.format(staging_server) 
 		else:
 			self.browser = webdriver.Chrome('/Users/justindodson/Desktop/WebDevelopment/obeyTheTestingGoat/tdd-book/superlists/functional_tests/chromedriver')
